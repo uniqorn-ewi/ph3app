74ecb8dec0ed9199e6d4cde395f0191240906ab6
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-06-09 11:06:38 +0900

    mail setting 4 heroku.

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,9 +1,9 @@
 class NotifyMailer < ApplicationMailer
-  default from: 'notifications@dummy.herokuapp.com'
+  default from: 'notifications@fast-earth-84235.herokuapp.com'
 
   def notify_mail(user)
     @user = user
-    @url  = 'https://dummy.herokuapp.com'
+    @url  = 'https://fast-earth-84235.herokuapp.com'
     mail to: "#{@user.email}", subject: "ブログの作成完了メール"
   end
 end
