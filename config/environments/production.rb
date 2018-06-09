fd7aa0c410d6c07ee88f6d190de479dd35d3397a
Yuji YAMAMOTO <oo_hayjp@yahoo.co.jp>
2018-06-09 10:53:18 +0900

    config 4 heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Settings for the mailer
-  config.action_mailer.default_url_options = { host: 'dummy.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'fast-earth-84235.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
